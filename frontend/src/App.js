cf8e8f858f8bbc0cea12547b996da185c835155a
Shay zak <97065139+ShayZak1@users.noreply.github.com>
2024-08-20 15:53:43 +0300

    m

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,67 +1,64 @@
 import { h } from 'preact';
 import { useState } from 'preact/hooks';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import ContactUs from "./components/ContactUs";
 import Hero from "./components/Hero";
 import Sonicintro from "./components/Sonicintro";
 import NavBar from "./components/NavBar";
 import TranslatorApp from "./components/Translator/TranslatorApp";
-import videoFile from './assets/img/TranslateBg.mp4';
 import VideoChat from './components/VideoChat/VideoChat';
+import videoFile from './assets/img/TranslateBg.mp4';
 
 const App = () => {
-  const [showTranslatorApp, setTranslatorApp] = useState(false);
-  const [showVideoChat, setShowVideoChat] = useState(false);
-
-  const handleVideoChat = () => {
-    setTranslatorApp(false);
-    setShowVideoChat(true);
-  };
-
   return (
-    <div className="relative w-full h-screen">
-      {showTranslatorApp ? (
-        <div className="translator-bg">
-          <video
-            style={{ filter: 'brightness(70%) blur(5px)' }}
-            autoPlay
-            muted
-            loop
-            playsInline
-            id="bg-video"
-            className="absolute top-0 left-0 w-full h-full object-cover"
-          >
-            <source src={videoFile} type="video/mp4" />
-          </video>
-          <div className="relative z-10 w-full h-full flex justify-center items-center">
-            <TranslatorApp onClose={() => setTranslatorApp(false)} />
-          </div>
-        </div>
-      ) : showVideoChat ? (
-        <div className="video-chat-bg">
-          <video
-            style={{ filter: 'brightness(70%) blur(5px)' }}
-            autoPlay
-            muted
-            loop
-            playsInline
-            id="bg-video"
-            className="absolute top-0 left-0 w-full h-full object-cover"
-          >
-            <source src={videoFile} type="video/mp4" />
-          </video>
-          <div className="relative z-10 w-full h-full flex justify-center items-center">
-            <VideoChat onClose={() => setShowVideoChat(false)} />
-          </div>
-        </div>
-      ) : (
-        <>
-          <NavBar onStart={() => setTranslatorApp(true)} onStartVideoChat={handleVideoChat} />
-          <Hero onStart={() => setTranslatorApp(true)} />
-          <Sonicintro onStart={() => setTranslatorApp(true)} />
-          <ContactUs onStart={() => setTranslatorApp(true)} />
-        </>
-      )}
-    </div>
+    <Router>
+      <div className="relative w-full h-screen">
+        <Switch>
+          <Route exact path="/">
+            <NavBar />
+            <Hero />
+            <Sonicintro />
+            <ContactUs />
+          </Route>
+          <Route path="/translatorapp">
+            <div className="translator-bg">
+              <video
+                style={{ filter: 'brightness(70%) blur(5px)' }}
+                autoPlay
+                muted
+                loop
+                playsInline
+                id="bg-video"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              >
+                <source src={videoFile} type="video/mp4" />
+              </video>
+              <div className="relative z-10 w-full h-full flex justify-center items-center">
+                <TranslatorApp />
+              </div>
+            </div>
+          </Route>
+          <Route path="/videochat">
+            <div className="video-chat-bg">
+              <video
+                style={{ filter: 'brightness(70%) blur(5px)' }}
+                autoPlay
+                muted
+                loop
+                playsInline
+                id="bg-video"
+                className="absolute top-0 left-0 w-full h-full object-cover"
+              >
+                <source src={videoFile} type="video/mp4" />
+              </video>
+              <div className="relative z-10 w-full h-full flex justify-center items-center">
+                <VideoChat />
+              </div>
+            </div>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 };
 
